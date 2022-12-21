08ba2ebb7217db59bce61b03408e89b3f7b3c179
jdane <julien.dane@gmail.com>
2022-12-21 11:59:09 +0100

    feat: add markdown on load

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// initial markdown
+const markdown = `
+# Je ne connais pas bien le markdown
+Je sais **écrire en gras**, _en italique_, **_les deux à la fois_**, ~~ou en barré~~.
+
+Je sais aussi \`// écrire du code en ligne\`
+
+et
+
+\`\`\`
+function say($what) {
+  echo $what;
+}
+say('ou bien sur plusieurs');
+\`\`\`
+(mais c'est un peu étrange).
+
+Je connais aussi
+- les listes
+  - que je peux indenter
+    - encore
+      - et encore
+1. Les listes ordonnées
+1. qu'on le veuille
+1. ou pas
+## Bien entendu
+### Toute une gamme
+##### De titres
+(les marges sont un peu abusées)
+
+Ah oui ! Je sais donner [un lien vers le site toto.com](https://www.toto.com)
+ 
+> et faire une citation ?
+
+(oui)
+
+et montrer l'image d'une loutre
+![image d'une loutre](./loutre.jpg)
+`;
+
 // app
 class App extends React.Component {
   constructor(props) {
@@ -6,6 +46,10 @@ class App extends React.Component {
       markdown: "",
     };
     this.handleMarkdownChange = this.handleMarkdownChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ markdown: markdown });
   }
 
   handleMarkdownChange(markdown) {
